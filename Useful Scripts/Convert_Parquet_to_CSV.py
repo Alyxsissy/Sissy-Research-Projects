998d96460b8d357d50b3b820516f5724ff8b3d4d
Nymbo <129332110+Nymbo@users.noreply.github.com>
2024-02-12 21:40:18 -0500

    adding more convert scripts and a batch file

---
+++ Useful Scripts/Convert_Parquet_to_CSV.py
@@ -0,0 +1,23 @@
+#remove the hashtags from lines 3, 4, and 5 the first time you run this script to install the required packages
+
+#pip install pandas
+#pip install pyarrow
+#pip install openpyxl
+
+import pandas as pd
+
+# Update the paths below to where your files are located in your Windows Downloads folder
+input_parquet_path = r'c:\Users\username\Downloads\FILENAME.parquet'  # Update this to your file's path
+output_csv_path = r'c:\Users\username\Downloads\output.csv'  # Update this to your desired output path
+
+# NOW YOU CAN RUN THE SCRIPT
+
+# THE OUTPUT FILE WILL BE LOCATED IN YOUR WINDOWS DOWNLOADS FOLDER BY DEFAULT
+
+# Reading the Parquet file
+df = pd.read_parquet(input_parquet_path)
+
+# Writing to an CSV file
+df.to_csv(output_csv_path, index=False)
+
+print(f"Successfully converted {input_parquet_path} to {output_csv_path}")
---
+++ Useful Scripts/Upload_Repo_to-Github.bat
@@ -0,0 +1,11 @@
+cd [LOCATION]
+
+git init
+
+git add .
+
+git commit -m "Initial commit"
+
+git remote add origin [HTTPS]
+
+git push -u origin master
